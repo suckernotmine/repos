cfc726d307f9782323217162386820bdf647c1ab
TheComputerGuy <spaceboyross@yandex.com>
2019-08-01 16:17:32 -0700

    Added build file

---
+++ build.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+repo=$1
+
+pkgCount=$(($(find "$repo" -type d | wc -l) - 1))
+pkgs=$(find "$repo" -type d | tail -$pkgCount | sed "s|$repo/||g" | sort)
+
+msg() {
+  printf "\033[0;32m==>\033[1;37m $@\n\033[0m"
+}
+
+msg "Found $pkgCount packages: $(echo ${pkgs[@]} | xargs)"
+
+[[ -d "out/$repo" ]] && rm -rf "out/$repo/"
+mkdir -p "out/$repo/"
+
+for pkg in ${pkgs[@]}; do
+  msg "Building package: $pkg"
+  pushd "$repo/$pkg"
+  makepkg
+  buildpkgs=$(find . -name "*.pkg.tar.xz")
+  for b in ${buildpkgs}; do
+    mv "$b" "../../out/$repo/$b"
+    repo-add "$repo.db.tar.gz" "$b"
+  done
+  folders=$(find . -type d | tail -$(($(find . -type d | wc -l) - 1)))
+  rm -rf "${folders[@]}"
+  popd
+done
+
+# vim:set ts=2 sw=2 et:
