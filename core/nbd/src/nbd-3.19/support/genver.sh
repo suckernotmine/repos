0d517dd92e4a3c00c008ac76ab00474e49a58c81
Tristan Ross <spaceboyross@yandex.com>
2019-09-01 01:27:05 -0700

    [core] deleting build files

--- core/nbd/src/nbd-3.19/support/genver.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-GITDESC=$(git describe --dirty|sed -e 's/nbd-//' 2>/dev/null)
-
-if [ -z "$GITDESC" ]; then
-	GITDESC="0.unknown"
-fi
-
-echo $GITDESC
--- core/nbd/src/nbd-3.19/tests/run/crypto-gnutls.c
+++
@@ -1,1 +0,0 @@
-../../crypto-gnutls.c
